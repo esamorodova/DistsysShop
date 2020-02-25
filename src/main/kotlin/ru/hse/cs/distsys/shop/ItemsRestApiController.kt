9f88724c6cf5703af8f771cd6d24fda015ac6ef6
katsam <katsam@passap.ru>
2020-02-26 00:21:08 +0300

    sample method

--- src/main/kotlin/ru/hse/cs/distsys/shop/ItemsRestApiController.kt
+++ src/main/kotlin/ru/hse/cs/distsys/shop/ItemsRestApiController.kt
@@ -2,10 +2,12 @@ package ru.hse.cs.distsys.shop
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
-@Controller("/api/items")
+@Controller
 class ItemsController {
     @GetMapping("/sample")
+    @ResponseBody
     fun sample(): Item = Item(1, "test", "test")
 }
 
