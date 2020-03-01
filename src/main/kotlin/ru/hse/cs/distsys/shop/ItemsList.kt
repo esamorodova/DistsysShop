74611bb112f8e9a2b7812fdad7ad87fe3da9b25d
katsam <katsam@passap.ru>
2020-03-01 21:29:40 +0300

    logger added

--- src/main/kotlin/ru/hse/cs/distsys/shop/ItemsList.kt
+++ src/main/kotlin/ru/hse/cs/distsys/shop/ItemsList.kt
@@ -1,5 +1,8 @@
 package ru.hse.cs.distsys.shop
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 interface ItemsRepository {
     fun addItem(name: String, category: String): Long
     fun deleteItem(id: Long)
@@ -12,16 +15,22 @@ class ListItemsRepository : ItemsRepository {
     private val items = mutableMapOf<Long, Item>()
     private var itemsCnt = 0L
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(ListItemsRepository::class.java)
+    }
+
     @Synchronized
     override fun addItem(name: String, category: String): Long {
         val id = itemsCnt++
         items[id] = Item(id, name, category)
+        logger.info("item added")
         return id
     }
 
     @Synchronized
     override fun deleteItem(id: Long) {
         items.remove(id)
+        logger.info("item deleted")
     }
 
     @Synchronized
@@ -32,6 +41,7 @@ class ListItemsRepository : ItemsRepository {
     @Synchronized
     override fun updateItem(item: Item) {
         items[item.itemId] = item
+        logger.info("item updated")
     }
 
     @Synchronized
